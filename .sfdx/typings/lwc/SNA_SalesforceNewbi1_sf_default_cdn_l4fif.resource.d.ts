e0ea9716caafc0328343e47c72c6af0f276edc81
Sanidhya Kuchhal <sanidhyakuchhal@gmail.com>
2022-07-28 16:05:26 +0530

    this is the first commit

---
+++ .sfdx/typings/lwc/SNA_SalesforceNewbi1_sf_default_cdn_l4fif.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/SNA_SalesforceNewbi1_sf_default_cdn_l4fif" {
+    var SNA_SalesforceNewbi1_sf_default_cdn_l4fif: string;
+    export default SNA_SalesforceNewbi1_sf_default_cdn_l4fif;
+}
